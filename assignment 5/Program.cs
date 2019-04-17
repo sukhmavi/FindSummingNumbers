3ec69268cf7b8bc01e061afd01de5347c3a48a86
visualstudio <visualstudio@122-PC27>
2019-04-17 09:14:12 -0400

    THIRD COMMIT

--- assignment 5/Program.cs
+++ assignment 5/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,21 @@ namespace assignment_5
     class GenerateList
     { const int LEN = 100;
         ArrayList listQ;
-        public void Run(int AddUpTonumbers) {
+        public void Run(int AddUpTonumbers)
+        {
             Random r = new Random();
 
             listQ = new ArrayList();
-            for (int i =0; i < LEN; i++) { listQ.Add(r.Next(100)); }
+            for (int i = 0; i < LEN; i++) { listQ.Add(r.Next(100)); }
             this.findTwoNumbersThatAddUpTo(AddUpTonumbers);
+        }
 
-}
+        public void findTwoNumbersThatAddUpTo(int addUpTonumbers)
+        {
+            throw new NotImplementedException();
+        }
     }
+    
     }
-}
+
+
